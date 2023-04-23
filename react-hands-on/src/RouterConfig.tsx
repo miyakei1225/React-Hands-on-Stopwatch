0f8729015bf5633954ec921323a35bf41e9c20aa
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2023-04-23 23:10:44 +0900

    Add: timer example

--- react-hands-on/src/RouterConfig.tsx
+++ react-hands-on/src/RouterConfig.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from 'pages/Home'
 import JSX from 'pages/JSX'
 import Chakra from 'pages/Chakra'
+import StopwatchExample from 'pages/StopWatchExample'
 
 export const RouterConfig = () => {
   return (
@@ -11,6 +12,7 @@ export const RouterConfig = () => {
           <Route path="/home" element={<Home />} />
           <Route path="/jsx" element={<JSX />} />
           <Route path="/chakra" element={<Chakra />} />
+          <Route path="/stopwatch/example" element={<StopwatchExample />} />
         </Routes>
       </BrowserRouter>
     </>
